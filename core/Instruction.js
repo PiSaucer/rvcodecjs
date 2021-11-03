a8a5ce41815a5580e842c1b4f4e72e9159b0b970
hikarisakai <hnsakai@ucdavis.edu>
2021-11-02 19:32:18 -0700

    Create Fragment class

--- core/Instruction.js
+++ core/Instruction.js
@@ -1,3 +1,19 @@
 export class Instruction {
 
 }
+
+class Fragment {
+    /**
+     * Represents a fragment of the instruction
+     * @param {String} assembly (ex. "x5")
+     * @param {String} bits (ex. "01101")
+     * @param {Number} index - start index of bits (ex. 7)
+     * @param {String} field (ex. "rd")
+     */
+    constructor(assembly, bits, index, field) {
+        this.assembly = assembly;
+        this.bits = bits;
+        this.index = index;
+        this.field = field;
+    }
+}
