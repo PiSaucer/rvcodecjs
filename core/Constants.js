09ca8db0b31f47743b4d9bc4fcdaa98a34e9b1b5
hikarisakai <hnsakai@ucdavis.edu>
2021-11-09 09:57:44 -0800

    Add constructor for Instruction class

---
+++ core/Constants.js
@@ -0,0 +1,17 @@
+export const BASE = {
+    BINARY: 2,
+    HEX: 16
+}
+
+// XLEN: width of an integer register in bits
+export const XLEN = {
+    RV32: 32
+}
+
+// Start and end indices of shared fields
+export const FIELD_COMMON = {
+    OPCODE: {
+        START: 0,
+        END: 6
+    }
+}
--- core/Instruction.js
+++ core/Instruction.js
@@ -1,5 +1,83 @@
-export class Instruction {
+import {BASE, FIELD_COMMON, XLEN} from './Constants.js'
 
+export class Instruction {
+    /**
+     * Creates an Instruction represented in multiple formats
+     * @param {String} instruction
+     */
+    constructor(instruction) {
+        // Create an array of fragments
+        this.fragments = [];
+
+        // Hard coded for now
+        this.isa = "RV32I";
+
+        // Determine format of instruction and decode
+        this.decodeInstruction(instruction);
+
+        // Print values of Instruction
+        console.log("ASSEMBLY: " + this.assembly);
+        console.log("BINARY: " + this.binary);
+        console.log("HEXADECIMAL: " + this.hex);
+        console.log("FORMAT: " + this.format);
+        console.log("SET: " + this.isa);
+        console.log(this.fragments);
+    }
+
+    // Check format of instruction and decode accordingly
+    decodeInstruction(instruction) {
+        // Regular expression for 32 bit binary instruction
+        var binaryRegEx = /^[01]{32}$/;
+        // Regular expression for 8 digit hexadecimal instruction
+        var hexRegEx = /^(0x)?[0-9a-fA-F]{8}$/;
+
+        // If instruction is in binary format
+        if (binaryRegEx.test(instruction)) {
+            this.binary = instruction;
+            // Convert to hex
+            this.hex = convertBinToHex(instruction);
+            // Convert to assembly
+            this.convertBinToAsm();
+        // If instruction is in hex format
+        } else if (hexRegEx.test(instruction)) {
+            this.hex = instruction;
+            // Convert to binary
+            this.binary = convertHexToBin(instruction);
+            // Convert to assembly
+            this.convertBinToAsm();
+        } else {
+            throw "Invalid instruction";
+        }
+    }
+
+    // Convert binary instruction to assembly
+    convertBinToAsm() {
+        // Use opcode to determine instruction type
+        this.opcode = this.getBits(FIELD_COMMON.OPCODE.START,
+                                    FIELD_COMMON.OPCODE.END);
+    }
+
+    // Given start and end index, return corresponding bits
+    getBits(indStart, indEnd) {
+        return this.binary.substring(XLEN.RV32 - (indEnd + 1),
+                                    XLEN.RV32 - indStart);
+    }
+}
+
+// Convert hexadecimal to 32 bit binary string
+function convertHexToBin(hex) {
+    var bin = parseInt(hex, BASE.HEX).toString(BASE.BINARY);
+    // Pad binary string with zeros while length < 32
+    return bin.padStart(32, '0');
+}
+
+// Convert binary to 8 digit hexadecimal
+function convertBinToHex(bin) {
+    var hex = parseInt(bin, BASE.BINARY).toString(BASE.HEX);
+    // Pad hex string with zeros while length < 8
+    hex = hex.padStart(8, '0');
+    // Add 0x prefix
+    return '0x' + hex;
 }
 
 class Fragment {
