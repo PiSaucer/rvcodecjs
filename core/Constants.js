024896e741089f45210e193d182a9de434ee82fd
hikarisakai <hnsakai@ucdavis.edu>
2021-11-09 14:03:51 -0800

    Add RTYPE decoder

--- core/Constants.js
+++ core/Constants.js
@@ -13,5 +13,37 @@ export const FIELD_COMMON = {
     OPCODE: {
         START: 0,
         END: 6
+    },
+
+    RD: {
+        START: 7,
+        END: 11
+    },
+
+    FUNCT3: {
+        START: 12,
+        END: 14
+    },
+
+    RS1: {
+        START: 15,
+        END: 19
+    },
+
+    RS2: {
+        START: 20,
+        END: 24
     }
+}
+
+// Start and end indices of R-TYPE fields
+export const FIELD_RTYPE = {
+    FUNCT7: {
+        START: 25,
+        END: 31
+    }
+}
+
+export const OPCODE = {
+    RTYPE:  '0110011'
 }
--- core/Instruction.js
+++ core/Instruction.js
@@ -1,4 +1,4 @@
-import {BASE, FIELD_COMMON, XLEN} from './Constants.js'
+import {BASE, FIELD_COMMON, XLEN, OPCODE, FIELD_RTYPE} from './Constants.js'
 
 export class Instruction {
     /**
@@ -55,6 +55,17 @@ export class Instruction {
         // Use opcode to determine instruction type
         this.opcode = this.getBits(FIELD_COMMON.OPCODE.START,
                                     FIELD_COMMON.OPCODE.END);
+
+        switch (this.opcode) {
+            // R-TYPE
+            case OPCODE.RTYPE:
+                this.decodeRType();
+                this.format = "R-TYPE";
+                break;
+            // Invalid opcode
+            default:
+                throw "Invalid opcode";
+        }
     }
 
     // Given start and end index, return corresponding bits
@@ -62,6 +73,89 @@ export class Instruction {
         return this.binary.substring(XLEN.RV32 - (indEnd + 1),
                                     XLEN.RV32 - indStart);
     }
+
+    /**
+     * Decodes R-type instruction
+     * @returns {String} output
+     */
+    decodeRType() {
+        // Define funct3 and funct7 bits
+        let rfunct = {  "0000000000": "ADD",
+                        "0000100000": "SUB",
+                        "0010000000": "SLL",
+                        "0100000000": "SLT",
+                        "0110000000": "SLTU",
+                        "1000000000": "XOR",
+                        "1010000000": "SRL",
+                        "1010100000": "SRA",
+                        "1100000000": "OR",
+                        "1110000000": "AND"
+        };
+
+        // Get bits for each field
+        var rd = this.getBits(FIELD_COMMON.RD.START, FIELD_COMMON.RD.END);
+        var funct3 = this.getBits(FIELD_COMMON.FUNCT3.START,
+                                    FIELD_COMMON.FUNCT3.END);
+        var rs1 = this.getBits(FIELD_COMMON.RS1.START, FIELD_COMMON.RS1.END);
+        var rs2 = this.getBits(FIELD_COMMON.RS2.START, FIELD_COMMON.RS2.END);
+        var funct7 = this.getBits(FIELD_RTYPE.FUNCT7.START,
+                                    FIELD_RTYPE.FUNCT7.END);
+
+        // Convert register numbers from binary to decimal
+        var reg1 = convertBinRegister(rs1);
+        var reg2 = convertBinRegister(rs2);
+        var destReg = convertBinRegister(rd);
+
+        // Check for operation in rfunct dictionary
+        var funct = funct3 + funct7;
+        if (funct in rfunct) {
+            var operation = rfunct[funct];
+        } else {
+            throw "Invalid funct3 or funct7";
+        }
+
+        // Create fragments for each field
+        this.fragments.push(new Fragment(operation, this.opcode,
+                            FIELD_COMMON.OPCODE.START, "opcode"));
+        this.fragments.push(new Fragment(operation, funct3,
+                            FIELD_COMMON.FUNCT3.START, "funct3"));
+        this.fragments.push(new Fragment(operation, funct7,
+                            FIELD_RTYPE.FUNCT7.START, "funct7"));
+        this.fragments.push(new Fragment(reg1, rs1,
+                            FIELD_COMMON.RS1.START, "rs1"));
+        this.fragments.push(new Fragment(reg2, rs2,
+                            FIELD_COMMON.RS2.START, "rs2"));
+        this.fragments.push(new Fragment(destReg, rd,
+                            FIELD_COMMON.RD.START, "rd"));
+
+        // Construct assembly instruction
+        this.assembly = renderAsmInstruction([operation, destReg, reg1, reg2]);
+    }
+}
+
+// Convert register numbers from binary to decimal
+function convertBinRegister(reg) {
+    return "x" + parseInt(reg, BASE.BINARY);
+}
+
+// Combine list of items into assembly instruction
+function renderAsmInstruction(list) {
+    // If list is empty, throw error
+    if (list.length < 1) {
+        throw "Empty list";
+    }
+
+    // Add first item
+    var output = list[0];
+
+    // If list has more than one item
+    if (list.length > 1) {
+        // Create sublist of following items
+        var items = list.splice(1);
+        // Add space before second item and separate next items with commas
+        output = output + " " + items.join(", ");
+    }
+    return output;
 }
 
 // Convert hexadecimal to 32 bit binary string
