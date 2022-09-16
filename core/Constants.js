a6ee40e5c678ce473bd60b167ff6eac8d80af5b2
Mattwmaster58 <mattwmaster58@gmail.com>
2022-09-15 22:51:55 -0600

    ft: interpret instructions which refer to registers by ABI names

--- core/Constants.js
+++ core/Constants.js
@@ -214,5 +214,41 @@ export const ISA_SYSTEM = {
     }
 }
 
+export const REGISTER = {
+  zero: "x0",
+  ra: "x1",
+  sp: "x2",
+  gp: "x3",
+  tp: "x4",
+  t0: "x5",
+  t1: "x6",
+  t2: "x7",
+  fp: "x8",
+  s0: "x8",
+  s1: "x9",
+  a0: "x10",
+  a1: "x11",
+  a2: "x12",
+  a3: "x13",
+  a4: "x14",
+  a5: "x15",
+  a6: "x16",
+  a7: "x17",
+  s2: "x18",
+  s3: "x19",
+  s4: "x20",
+  s5: "x21",
+  s6: "x22",
+  s7: "x23",
+  s8: "x24",
+  s9: "x25",
+  s10: "x26",
+  s11: "x27",
+  t3: "x28",
+  t4: "x29",
+  t5: "x30",
+  t6: "x31"
+}
+
 // Entire ISA
 export const ISA = Object.assign({}, ISA_RV32I);
