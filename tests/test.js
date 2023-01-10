c55630fba460de7fafcbd60c284caeaa0296b105
nkrim62 <nkrim62@gmail.com>
2023-01-09 22:10:53 -0800

    test.js: add batchTests()

--- tests/test.js
+++ tests/test.js
@@ -19,8 +19,39 @@ export function test(statement, testCase) {
   try {
     testCase();
   } catch (e) {
-    console.log(e.message);
+    console.log(e.message ?? e);
     return;
   }
   console.log(' ✓');
+}
+
+/**
+ * @param {string} statement
+ * @param {Function} test case function
+ */
+export function batchTests(batchStatement, testCases) {
+  console.log(`${batchStatement}: ${testCases.length} tests`);
+  let failCount = 0;
+  for (const [s, t] of testCases) {
+    try {
+      const res = t();
+      process.stdout.write('✓');
+    } catch (e) {
+      ++failCount;
+      console.log('');
+      process.stdout.write(s + ': ');
+      if (e.message) {
+        console.log(e.message);
+      } else {
+        console.log('\n\t' + e);
+      }
+    }
+  }
+  if (failCount === 0) {
+    console.log('');
+    console.log(`All ${testCases.length} tests passed!`);
+  } else {
+    console.log('');
+    console.log(`${failCount}/${testCases.length} tests failed...`);
+  }
 }
